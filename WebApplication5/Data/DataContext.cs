31988dad6573c22ef11a57a1935697240bc2dcd1
sevam <sevam@SEVA>
2024-10-16 17:04:27 +0400

    Add project files.

---
+++ WebApplication5/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication5.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<>) 
+        {
+        }
+     
+    }
+}
